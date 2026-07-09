19bf859e001106aa357fbde8c00d4b87bd13c63a
andrenevesxavier <andrenevesxavier276@gmail.com>
2026-07-09 20:45:20 -0300

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/G9_BR_EQUIPE_20/G9BrEquipe20Application.java
@@ -0,0 +1,13 @@
+package com.G9_BR_EQUIPE_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G9BrEquipe20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G9BrEquipe20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/G9_BR_EQUIPE_20/G9BrEquipe20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G9_BR_EQUIPE_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G9BrEquipe20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
